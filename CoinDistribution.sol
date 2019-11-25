2a2fb1025ede9095ff5d8bbe5a2dff860a2ee172
twrbo <b3751282@gmail.com>
2019-11-25 16:51:51 +0800

    second commit

--- CoinDistribution.sol
+++ CoinDistribution.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.4.22 <0.6.0;
 
 contract CoinDistribution{
     address owner;
-    uint public NumOfAccounts;
+    uint NumOfAccounts;
     mapping(address => uint256) accountid;
     address payable[] accounts;
     uint PriceLimit=100 ether;
@@ -12,36 +12,29 @@ contract CoinDistribution{
     }
     
     modifier OnlyOwner(){
-        require(msg.sender==owner);
+        require(msg.sender==owner,"Admin only!");
         _;
     }
     
-    function Deposit() public payable{
-        require(msg.value>0 && msg.value < PriceLimit,"Deposit at most 100 ether!");
-    }
-    
-    function SplitAllMoney()public payable OnlyOwner{
-        uint ShareMoney=address(this).balance/(accounts.length);
-        for (uint i=0; i<accounts.length;i++){
-            accounts[i].transfer(ShareMoney);
+    modifier OnlyMember(){
+        if (accounts.length==0){
+            require(msg.sender==owner,"Member only!");
         }
+        else    
+        {
+            for(uint i=0; i<accounts.length;i++){
+                require(msg.sender==accounts[i] || msg.sender==owner,"Member only!"); 
+            }
+        }
+        _;
     }
     
-    
-    function ReturnAllMoney()public payable OnlyOwner{
-        msg.sender.transfer(address(this).balance);
-    }
-    
-    function GetBalance() public view OnlyOwner returns(uint)  {
-         return address(this).balance;
-    }
-
-    //input format ["0x000000000,0x000000000"]
-    function AddAccount(address payable[]memory _accountAddress) OnlyOwner public {
+    //input format ["0x000000000","0x000000000"]
+    function _AddAccount(address payable[]memory _accountAddress) OnlyOwner public {
         //account repeat or not
         for (uint i=0; i<accounts.length;i++){
             for (uint u=0; u<_accountAddress.length;u++){
-                require(_accountAddress[u]!=accounts[i],"repaet account");
+                require(_accountAddress[u]!=accounts[i],"Repaet account!");
             }
         }
         for (uint i=0;i<_accountAddress.length;i++){
@@ -54,11 +47,11 @@ contract CoinDistribution{
             NumOfAccounts++;
             //accounts[id].transfer(1 ether); //give 1 ether after add account
         }
-        
     }
     
     //input format 0x000000000
-    function RemoveAccount(address _accountAddress) OnlyOwner public {
+    function _RemoveAccount(address _accountAddress) OnlyOwner public {
+        require(NumOfAccounts>0,"No accounts!");
         for (uint i=0; i<accounts.length;i++){
             if(accounts[i]==_accountAddress){
                 delete accounts[i];
@@ -71,9 +64,73 @@ contract CoinDistribution{
             }
         }
     }
+    
+    function _RemoveAllAccount() OnlyOwner public {
+        require(NumOfAccounts>0,"No accounts!");
+        for (uint i=0; i<accounts.length;i++){
+            delete accountid[accounts[i]];
+            NumOfAccounts--;
+        }
+        delete accounts;
+    }
    
-   function AllAccountID()public view returns(address payable[] memory){
+    function _ReturnAllMoney()public payable OnlyOwner{
+        require(address(this).balance > 0,"No money!");
+        msg.sender.transfer(address(this).balance);
+    }
+   
+    function _SplitAllMoney()public payable OnlyOwner{
+        require(address(this).balance > 0,"No money!");
+        uint ShareMoney=address(this).balance/(accounts.length);
+        for (uint i=0; i<accounts.length;i++){
+            accounts[i].transfer(ShareMoney);
+        }
+    }
+   
+    function AddYourAccountOnly () public{
+        for (uint i=0; i<accounts.length;i++){
+            require(msg.sender!=accounts[i],"Repaet account!");
+        }
+        uint id = accountid[msg.sender];
+        if (id == 0) {
+            accountid[msg.sender] = accounts.length;
+            id = accounts.length++;
+        }
+        accounts[id] = msg.sender;
+        NumOfAccounts++;
+    }
+    
+    function DeleteYourAccountOnly() public{
+        require(accounts.length>0,"No accounts!");
+        for (uint i=0; i<accounts.length;i++){
+            require(accounts[i]==msg.sender,"Not in this contract!");
+            if(accounts[i]==msg.sender){
+                delete accounts[i];
+                delete accountid[msg.sender];
+                NumOfAccounts--;
+                for (uint u = i; u<accounts.length-1; u++){
+                    accounts[u] = accounts[u+1];
+                }
+                accounts.length--;
+            }
+        }
+    }
+    
+    function Deposit() public payable{
+        require(msg.value>0 && msg.value < PriceLimit,"Deposit at most 100 ether!");
+    }
+   
+    function GetAllAccountID()public OnlyMember view returns(address payable[] memory){
            return accounts;
    }
+   
+       
+    function GetBalance() public OnlyMember view returns(uint)  {
+         return address(this).balance;
+    }
+    
+    function GetNumOfAccounts() public OnlyMember view returns(uint)  {
+         return NumOfAccounts;
+    }
 
 }
